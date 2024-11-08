85585b863bd2e8039eab3207dd42ca593395e6d1
Andrey Masunov <andrey.masunov@dci-student.org>
2024-11-08 11:48:13 +0100

    initial commit

---
+++ src/main/java/org/dci/theratrack/TheraTrackApplication.java
@@ -0,0 +1,13 @@
+package org.dci.theratrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheraTrackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TheraTrackApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/dci/theratrack/TheraTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dci.theratrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheraTrackApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
