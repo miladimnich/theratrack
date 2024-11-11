3f6d635c3ee3e008b4b42a2a66a3d4e733242ae2
Andrey Masunov <andrey.masunov@dci-student.org>
2024-11-11 15:11:20 +0100

    THER-43 added repositories for entities

---
+++ src/main/java/org/dci/theratrack/repository/AppointmentRepository.java
@@ -0,0 +1,9 @@
+package org.dci.theratrack.repository;
+
+import org.dci.theratrack.entity.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+}
