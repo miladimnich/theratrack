069d2d499c3a129920aba13e8bbeb8566f982537
Andrey Masunov <andrey.masunov@dci-student.org>
2024-11-14 13:13:54 +0100

    THER-49 added basic service layers for Appointments, diagnosis, treatment

--- src/main/java/org/dci/theratrack/repository/TherapistRepository.java
+++ src/main/java/org/dci/theratrack/repository/TherapistRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TherapistRepository extends JpaRepository<Therapist, Integer> {
+public interface TherapistRepository extends JpaRepository<Therapist, Long> {
 }
