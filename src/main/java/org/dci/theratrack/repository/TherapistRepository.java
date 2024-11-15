1707020e0dccee5f2e7f8e1c21679d174bca24b7
imanRagab <iman.mansour94@gmail.com>
2024-11-15 15:21:28 +0100

    Fix TherapistRepository bug

--- src/main/java/org/dci/theratrack/repository/TherapistRepository.java
+++ src/main/java/org/dci/theratrack/repository/TherapistRepository.java
@@ -1,6 +1,8 @@
 package org.dci.theratrack.repository;
 
 import org.dci.theratrack.entity.Therapist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 
 public interface TherapistRepository extends JpaRepository<Therapist, Long>{
 }
