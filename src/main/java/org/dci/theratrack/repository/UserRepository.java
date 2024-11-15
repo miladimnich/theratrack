9c0b2caf6a0b3d09a21a21543a935889a1cdccd9
imanRagab <iman.mansour94@gmail.com>
2024-11-15 15:20:40 +0100

    Fix UserRepository bug

--- src/main/java/org/dci/theratrack/repository/UserRepository.java
+++ src/main/java/org/dci/theratrack/repository/UserRepository.java
@@ -1,8 +1,9 @@
 package org.dci.theratrack.repository;
 
+import org.dci.theratrack.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface User extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
