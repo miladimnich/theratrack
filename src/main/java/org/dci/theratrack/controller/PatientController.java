357ca3c0c6c723315aafc4429ef836cb186317e1
maryna <maryna.dymnich@dci-student.org>
2024-11-23 22:18:05 +0100

    added getTherapySessionHistory

--- src/main/java/org/dci/theratrack/controller/PatientController.java
+++ src/main/java/org/dci/theratrack/controller/PatientController.java
@@ -1,13 +1,12 @@
 package org.dci.theratrack.controller;
 
 import org.dci.theratrack.entity.Patient;
-import org.dci.theratrack.request.AppointmentDTO;
 import org.dci.theratrack.request.PatientRequest;
-import org.dci.theratrack.request.TherapySessionHistoryDTO;
+import org.dci.theratrack.request.TherapySessionHistoryRequest;
+import org.dci.theratrack.service.AppointmentService;
 import org.dci.theratrack.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,6 +18,8 @@ public class PatientController {
   @Autowired
   private PatientService patientService;
 
+  @Autowired
+  private AppointmentService appointmentService;
   @PostMapping
   public ResponseEntity<Patient> createPatient(@RequestBody PatientRequest request) {
     return ResponseEntity.ok(patientService.createPatient(request));
@@ -45,6 +46,13 @@ public class PatientController {
   public ResponseEntity<Void> deletePatient(@PathVariable Long id) {
     patientService.deletePatient(id);
     return ResponseEntity.noContent().build();
+  }
+
+
+  @GetMapping("/history/{patientId}")
+  public ResponseEntity<List<TherapySessionHistoryRequest>> getTherapySessionHistory(@PathVariable Long patientId) {
+    List<TherapySessionHistoryRequest> history = patientService.getTherapySessionHistory(patientId);
+    return ResponseEntity.ok(history);
   }
 }
 
