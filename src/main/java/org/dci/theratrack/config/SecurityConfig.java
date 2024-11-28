b443d51fa8e8833b85a4c0f0083cecabe3c3192b
imanRagab <iman.mansour94@gmail.com>
2024-11-28 15:58:46 +0100

    Fix Cors-origin Problem

--- src/main/java/org/dci/theratrack/config/SecurityConfig.java
+++ src/main/java/org/dci/theratrack/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.dci.theratrack.enums.UserRole;
 import org.dci.theratrack.filter.JwtRequestFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -15,7 +16,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtRequestFilter jwtRequestFilter) throws Exception {
         http.csrf(csrf -> csrf.disable()) // Disable CSRF
-            .authorizeHttpRequests((requests) -> requests
+            .authorizeHttpRequests((requests) -> requests.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Allow OPTIONS requests
             .requestMatchers("/api/auth/**").permitAll()
             .requestMatchers("/api/patients/**").hasRole(UserRole.ADMIN.name())
             .requestMatchers("/api/therapists/**").hasRole(UserRole.ADMIN.name())
